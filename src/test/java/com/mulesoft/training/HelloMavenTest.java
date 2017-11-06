1710332d6d7a83b09a00b3be011d7edc14308b9d
liweitao041745 <liweitao041745@163.com>
2017-11-06 09:59:20 +0800

    modify payload

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -9,7 +9,7 @@ public class HelloMavenTest extends FunctionalTestCase {
 
     @Test
     public void mavenFlowReturnsHelloMaven() throws Exception {
-        runFlowAndExpect("mavenFlow", "hello ,maven.");
+        runFlowAndExpect("mavenFlow", "hello ,maven2.");
     }
     
     @Override
